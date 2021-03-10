9c6f18dfea84ea0b1063195633aa714edc46af4d
lena-r-a <rizhankova.elena@gmail.com>
2021-03-10 14:20:02 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var _n= Math.abs(n);
+  var str = String(_n);
+  var arr = str.split('');
+ var k = Number(arr.reverse().join(''));
+  return k;
+
 }
